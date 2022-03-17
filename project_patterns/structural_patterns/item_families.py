054e2bbbd662f6856f2a770c6d223a8d5b6a5630
kaslufl <leallucasfonseca@gmail.com>
2022-03-17 16:43:54 -0300

    feat: add flyweight pattern with test

---
+++ project_patterns/structural_patterns/item_families.py
@@ -0,0 +1,22 @@
+from .item import Item
+
+
+class ItemFamilies:
+    "A dict to store item data"
+
+    item_families = {}
+
+    def  __new__(cls, name: str, item_family_id: int) -> None:
+        try:
+            id = cls.item_families[item_family_id]
+        except KeyError:
+            id = object.__new__(cls)
+            cls.item_families[item_family_id] = id
+        return id
+
+    def set_item_info(self, item_info):
+        item = Item()
+        self.item_info = item.get_name(item_info)
+
+    def get_item_info(self):
+        return self.item_info
