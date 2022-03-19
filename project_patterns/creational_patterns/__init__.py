f34f9ea05a01585bc7910fb184b2a705df093f4a
kaslufl <leallucasfonseca@gmail.com>
2022-03-19 16:55:13 -0300

    refact: change singleton test method and rename test factory

--- project_patterns/creational_patterns/__init__.py
+++ project_patterns/creational_patterns/__init__.py
@@ -2,3 +2,4 @@ from .builder import *
 from .prototype import *
 from .factory import *
 from .abstract_factory import *
+from .singleton import *
--- project_patterns/creational_patterns/singleton/logger.py
+++ project_patterns/creational_patterns/singleton/logger.py
@@ -1,19 +1,4 @@
-from threading import Lock, Thread
-
-
-class LoggerMeta(type):
-    """Consists in a thread-safe singleton implementation."""
-
-    _instances = {}
-
-    _lock: Lock = Lock()
-
-    def __call__(cls, *args, **kwargs):
-        with cls._lock:
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
-        return cls._instances[cls]
+from .logger_meta import LoggerMeta
 
 
 class Logger(metaclass=LoggerMeta):
@@ -30,11 +15,4 @@ class Logger(metaclass=LoggerMeta):
 
 def test_logger(value: str) -> None:
     logger = Logger(value)
-    print(logger.get_logger())
-
-
-if __name__ == "__main__":
-    process1 = Thread(target=test_logger, args=("Logger",))
-    process2 = Thread(target=test_logger, args=("NotLogger",))
-    process1.start()
-    process2.start()
+    return logger.get_logger()
